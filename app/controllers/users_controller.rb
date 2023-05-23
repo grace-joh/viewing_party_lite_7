60dcefad7adc9a44facd48e6be39cccf727d4c6e
Grace Joh <graceehjoh@gmail.com>
2023-05-23 12:08:09 -0500

    Refactor: with Sessions controller from Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,20 +26,6 @@ class UsersController < ApplicationController
     # end
   end
 
-  def login_form
-  end
-
-  def login
-    user = User.find_by(email: user_params[:email])
-    if user.authenticate(user_params[:password])
-      session[:user_id] = user.id
-      redirect_to user_path(user)
-    else
-      flash[:error] = "Sorry, your credentials are bad."
-      render :login_form
-    end
-  end
-
   private
   def user_params
     params.permit(:user_name, :email, :password, :password_confirmation)
